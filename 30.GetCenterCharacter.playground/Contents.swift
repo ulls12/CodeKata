3f68151720733b7cf08b091bdc5e12e36f3974c0
ulls12 <koes823@gmail.com>
2023-12-18 09:52:59 +0900

    1

--- 30.GetCenterCharacter.playground/Contents.swift
+++ 30.GetCenterCharacter.playground/Contents.swift
@@ -1,3 +1,9 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보자
+// 단어의 길이가 짝수라면 가운데 두 글자를 반환하면 된다
+
+func solution(_ s:String) -> String {
+    return s.count % 2 == 0 ? String(Array(s)[(s.count/2)-1]) + String(Array(s)[s.count/2]) : String(Array(s)[s.count/2])
+}
+
